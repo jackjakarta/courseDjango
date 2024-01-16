adcf540b043108889903b8c44c2b59e25c4a03b1
jackjakarta <al.termure@gmail.com>
2024-01-16 02:37:12 +0100

    added django action test

--- hr/tests.py
+++ hr/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.http import HttpResponse
+from .views import home_view
 
-# Create your tests here.
+
+class HomeViewTest(TestCase):
+    def test_home_view_status_code(self):
+        response = self.client.get(reverse('home'))  # Use the URL pattern name 'home' here
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_view_template_used(self):
+        response = self.client.get(reverse('home'))  # Use the URL pattern name 'home' here
+        self.assertTemplateUsed(response, 'home.html')  # Replace 'myapp/home.html' with your actual template path
