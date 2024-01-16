94bb0efda37633f4a9d396a956679c29239e3500
jackjakarta <al.termure@gmail.com>
2024-01-16 03:54:30 +0100

    test functions

--- hr_manager/settings.py
+++ hr_manager/settings.py
@@ -102,17 +102,13 @@ WSGI_APPLICATION = 'hr_manager.wsgi.application'
 #         'NAME': config('DB_NAME'),
 #         'USER': config('DB_USER'),
 #         'PASSWORD': config('DB_PASSWORD')
-#     },
-#     'test': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
